23361bee0f3b6776796cddd03a70450f3758d1d1
AlulCode45 <alulgans70@gmail.com>
2023-09-24 07:07:43 +0700

    [UPDATE] Adding sertifikat& Project: 	modified:   .eslintrc.json 	modified:   .gitignore 	modified:   README.md 	modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   postcss.config.js 	modified:   public/favicon.ico 	modified:   public/next.svg 	new file:   public/project/AlulCode-Portfolio-Website.png 	new file:   public/project/Aplikasi-BKK-SMKN-4-BOJONEGORO.png 	new file:   public/project/Aplikasi-Keanggotaan-ASPIKOM.png 	new file:   public/project/Buku-induk.png 	new file:   public/project/Bursa-Kerja-Khusus-SMKN-4-Bojonegoro.png 	new file:   public/project/Dashboard-mindZets.png 	new file:   public/project/Dharma-Wanita-Persatuan-Dinas-Pendidikan-Provinsi-Jawa-Timur.png 	new file:   public/project/UD-Sumber-Rejeki.png 	modified:   public/sertifikat/Juara 1.png 	new file:   public/sertifikat/Muhammad Khoyron Ahlaqul Firdaus - Sertifikat ADSE 2023-1.png 	new file:   public/sertifikat/Muhammad Khoyron_Ahlaqul F -af0a7169-4377-c803-2784-6aadd15ce71f.png 	new file:   public/sertifikat/WhatsApp Image 2023-07-14 at 19.46.17.jpeg 	new file:   public/sertifikat/cert_Informatika - OSPN 2023.png 	new file:   public/sertifikat/flutter-mobile-apps-muhammad-khoyron-ahlaqul-f.png 	new file:   public/sertifikat/khoyron.png 	new file:   public/sertifikat/sertifikat_course_123_2211923_190923133742-1.png 	new file:   public/sertifikat/sertifikat_course_256_2211923_251022151156-1.png 	modified:   public/sertifikat/sertifikat_course_256_2211923_251022151156.png 	new file:   public/sertifikat/sertifikat_course_315_2211923_230923191829-1.png 	new file:   public/sertifikat/sertifikat_course_403_2211923_170923192811-1.png 	new file:   public/sertifikat/zero-to-hero-10-tahun-berkarier-sebagai-developer-certificate.png 	modified:   public/thirteen.svg 	modified:   public/vercel.svg 	modified:   src/pages/_app.tsx 	modified:   src/pages/_document.tsx 	modified:   src/pages/index.tsx 	modified:   src/styles/Home.module.css 	modified:   src/styles/globals.css 	modified:   tailwind.config.js 	modified:   tsconfig.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,7 @@ import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper";
 import DocViewer, { DocViewerRenderers } from "@cyntler/react-doc-viewer";
 import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -29,6 +30,36 @@ export default function Home() {
   ]
   const sertif5 = [
     { uri: "/sertifikat/gamelab_certificate_vscode.png" },
+  ]
+  const sertif6 = [
+    { uri: "/sertifikat/cert_Informatika - OSPN 2023.png" },
+  ]
+  const sertif7 = [
+    { uri: "/sertifikat/flutter-mobile-apps-muhammad-khoyron-ahlaqul-f.png" },
+  ]
+  const sertif8 = [
+    { uri: "/sertifikat/khoyron.png" },
+  ]
+  const sertif9 = [
+    { uri: "/sertifikat/Muhammad Khoyron Ahlaqul Firdaus - Sertifikat ADSE 2023-1.png" },
+  ]
+  const sertif10 = [
+    { uri: "/sertifikat/Muhammad Khoyron_Ahlaqul F -af0a7169-4377-c803-2784-6aadd15ce71f.png" },
+  ]
+  const sertif11 = [
+    { uri: "/sertifikat/sertifikat_course_123_2211923_190923133742-1.png" },
+  ]
+  const sertif12 = [
+    { uri: "/sertifikat/sertifikat_course_315_2211923_230923191829-1.png" },
+  ]
+  const sertif13 = [
+    { uri: "/sertifikat/sertifikat_course_403_2211923_170923192811-1.png" },
+  ]
+  const sertif14 = [
+    { uri: "/sertifikat/WhatsApp Image 2023-07-14 at 19.46.17.jpeg" },
+  ]
+  const sertif15 = [
+    { uri: "/sertifikat/zero-to-hero-10-tahun-berkarier-sebagai-developer-certificate.png" },
   ]
 
   const [windowWidth, setWindowWidth] = useState("lg");
@@ -123,10 +154,6 @@ export default function Home() {
               </ul>
             </div>
           </nav>
-
-
-
-
           <div className="flex h-[calc(100vh-50px)] justify-center items-center">
             <div className="md:w-[700px] text-center">
               <h1 className='md:text-[63px] text-3xl text-center font-bold md:leading-[70px]'>Hi! i’am <b className='text-primary'>Muhammad Khoyron Ahlaqul F</b></h1>
@@ -187,6 +214,9 @@ export default function Home() {
               <div className="bg-[#073B4C] px-10 py-1  text-white rounded text-center">
                 <h1>HTML</h1>
               </div>
+              <div className="bg-[#06D6A0] px-10 py-1  text-white rounded text-center">
+                <h1>Flutter</h1>
+              </div>
             </div>
             <div className="md:flex grid grid-cols-1 gap-3 w-full mt-3">
               <div className="bg-[#06D6A0] px-10 py-1  text-white rounded text-center">
@@ -194,6 +224,12 @@ export default function Home() {
               </div>
               <div className="bg-[#EF476F] px-10 py-1  text-white rounded text-center">
                 <h1>JQuery</h1>
+              </div>
+              <div className="bg-[#F8CB64] px-10 py-1  text-white rounded text-center">
+                <h1>Express JS</h1>
+              </div>
+              <div className="bg-[#00AFB9] px-10 py-1  text-white rounded text-center">
+                <h1>Python</h1>
               </div>
             </div>
           </div>
@@ -247,6 +283,96 @@ export default function Home() {
                 }
               }} />
             </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif6} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif7} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif8} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif9} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif10} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif11} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif12} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif13} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif14} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
+            <SwiperSlide className="bg-gray-200 rounded-3xl">
+              <DocViewer documents={sertif15} pluginRenderers={DocViewerRenderers} config={{
+                header: {
+                  disableHeader: true,
+                  disableFileName: true,
+                  retainURLParams: true,
+                }
+              }} />
+            </SwiperSlide>
           </Swiper>
         </section>
 
@@ -254,32 +380,32 @@ export default function Home() {
           <h1 className='text-3xl font-bold'>Projects</h1>
           <p className='my-5 md:w-[650px]'>Semua project ini telah saya selesaikan dalam kurun waktu saya belajar dan beberapa di antaranya adalah project dari perusahaan maupun perorangan.</p>
           <Swiper
-            slidesPerView={(windowWidth == "sm" || windowWidth == "md") ? 1 : (windowWidth == "lg" ? 4 : 4)}
+            slidesPerView={1}
             spaceBetween={30}
             navigation={true}
             pagination={{
               clickable: true,
             }}
             modules={[Pagination, Navigation]}
-            className="h-[300px] mt-10"
+            className="h-[600px] mt-10"
           >
             <SwiperSlide className='bg-gray-200 p-4 rounded-xl text-center'>
-              Project 1
+              <Image src={'/project/Aplikasi-Keanggotaan-ASPIKOM.png'} alt='' fill={true} />
             </SwiperSlide>
             <SwiperSlide className='bg-gray-200 p-4 rounded-xl text-center'>
-              Project 2
+              <Image src={'/project/Dharma-Wanita-Persatuan-Dinas-Pendidikan-Provinsi-Jawa-Timur.png'} alt='' fill={true} />
             </SwiperSlide>
             <SwiperSlide className='bg-gray-200 p-4 rounded-xl text-center'>
-              Project 3
+              <Image src={'/project/UD-Sumber-Rejeki.png'} alt='' fill={true} />
             </SwiperSlide>
             <SwiperSlide className='bg-gray-200 p-4 rounded-xl text-center'>
-              Project 4
+              <Image src={'/project/Dashboard-mindZets.png'} alt='' fill={true} />
             </SwiperSlide>
             <SwiperSlide className='bg-gray-200 p-4 rounded-xl text-center'>
-              Project 5
+              <Image src={'/project/Aplikasi-BKK-SMKN-4-BOJONEGORO.png'} alt='' fill={true} />
             </SwiperSlide>
             <SwiperSlide className='bg-gray-200 p-4 rounded-xl text-center'>
-              Project 6
+              <Image src={'/project/Buku-induk.png'} alt='' fill={true} />
             </SwiperSlide>
           </Swiper>
         </section>
@@ -345,7 +471,7 @@ export default function Home() {
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
                     clip-rule="evenodd"
                   />
@@ -368,7 +494,7 @@ export default function Home() {
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
                     clip-rule="evenodd"
                   />
@@ -384,7 +510,7 @@ export default function Home() {
                 className="text-gray-700 transition hover:text-gray-700/75"
               >
                 <span className="sr-only">Email</span>
-                <svg fill="#000000" width="24px" height="24px" viewBox="0 0 0.72 0.72" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M0.63 0.221V0.15H0.09v0.071l0.27 0.135 0.27 -0.135Zm0 0.067 -0.27 0.135 -0.27 -0.135V0.57h0.54V0.289ZM0.09 0.09h0.54a0.06 0.06 0 0 1 0.06 0.06v0.42a0.06 0.06 0 0 1 -0.06 0.06H0.09a0.06 0.06 0 0 1 -0.06 -0.06V0.15a0.06 0.06 0 0 1 0.06 -0.06Z" /></svg>
+                <svg fill="#000000" width="24px" height="24px" viewBox="0 0 0.72 0.72" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M0.63 0.221V0.15H0.09v0.071l0.27 0.135 0.27 -0.135Zm0 0.067 -0.27 0.135 -0.27 -0.135V0.57h0.54V0.289ZM0.09 0.09h0.54a0.06 0.06 0 0 1 0.06 0.06v0.42a0.06 0.06 0 0 1 -0.06 0.06H0.09a0.06 0.06 0 0 1 -0.06 -0.06V0.15a0.06 0.06 0 0 1 0.06 -0.06Z" /></svg>
               </a>
             </li>
 
@@ -403,7 +529,7 @@ export default function Home() {
                   aria-hidden="true"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
                     clip-rule="evenodd"
                   />
